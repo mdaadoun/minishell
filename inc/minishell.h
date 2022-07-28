0fe0febfae205b7b9b5ab2f22b02b3cbc3a2cb90
mdaadoun <daadounmichael@gmail.com>
2022-07-28 13:59:36 +0200

    update ms header

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,11 +1,27 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-//include
+// libraries
+
 # include "../src/libft/libft.h"
 
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <errno.h>
+# include <error.h>
+# include <getopt.h>
+# include <signal.h>
+
+// types
+
+typedef signed char			t_int8;
+typedef unsigned char		t_uint8;
+typedef signed short		t_int16;
+typedef unsigned short		t_uint16;
+typedef signed long			t_int32;
+typedef unsigned long		t_uint32;
+typedef signed long long	t_int64;
+typedef unsigned long long	t_uint64;
 
 #endif
