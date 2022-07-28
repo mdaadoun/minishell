0564984469859bfdcf043596b16a4f1fd2c022f0
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-07-28 13:38:44 +0200

    main

--- src/main.c
+++ src/main.c
@@ -1,19 +1,32 @@
 #include "../inc/minishell.h"
+#include <errno.h>
+#include <error.h>
+#include <getopt.h>
+#include <signal.h>
+
+void	handle_signals(int signo)
+{
+	if (signo == SIGINT)
+		printf("\nYou pressed Ctrl+C\n|-->");
+}
 
 int	main(int ac, char **av)
 {
-	char	*test;
-	int	count;
+	char	*input;
 
-	count = 0;
-	while (count < 10)
+	if (signal(SIGINT, handle_signals) == SIG_ERR)
+		printf("Failed\n");
+	while (1)
 	{
-		test = readline("|-->");
-		printf("%s\n", test);
-		if (ft_strncmp(test, "exit", 4))
-			count = 10;
-		free(test);
-		count++;
+		input = readline("|-->");
+		if (!input)
+			break;
+		if (!ft_strncmp(input, "exit", 4))
+		{
+			free(input);
+			break;
+		}
+		free(input);
 	}
 	(void)ac;
 	(void)av;
