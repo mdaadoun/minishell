66d56e8b4dfa335df81f44da623b26f68f9e0709
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-08-04 12:02:36 +0200

    Updated Test Parser with argument

--- src/test/test_parser.c
+++ src/test/test_parser.c
@@ -24,35 +24,69 @@ void launch_parser(t_minishell *ms, char *line)
     ft_printf("\n");
 }
 
+void	test_quote(t_minishell *ms)
+{
+	printf("===Testing quotes parser===\n");
+	ms->full_line = ft_strdup("'echo lol'");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+	ms->full_line = ft_strdup("'hello' hello hello");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("'hello'");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("'he\"l\"lo'");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("test'123\"456\"123'test");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("test\"123'456'123\"test");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+}
+
+void	test_env(t_minishell *ms)
+{
+	printf("===Testing env parser===\n");
+	ms->full_line = ft_strdup("echo $PAGER");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+	ms->full_line = ft_strdup("echo $BA");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+}
+
+void	test_pipe(t_minishell *ms)
+{
+	printf("===Testing pipe parser===\n");
+	ms->full_line = ft_strdup("cat Makefile|wc -l");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("cat Makefile | wc -l");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("cat 'Makefile|wc' -l");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+    ms->full_line = ft_strdup("|wc -l");
+	launch_parser(ms, ms->full_line);
+	free(ms->full_line);
+}
+
 void test_parser(t_minishell *ms, int debug)
 {
+	if (debug == TEST_PARSER_QUOTES)
+		test_quote(ms);
+	if (debug == TEST_PARSER_ENV)
+		test_env(ms);
+	if (debug == TEST_PARSER_PIPES)
+		test_pipe(ms);
     if (debug == TEST_PARSER)
     {
-        if (ms->full_line)
-            launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("'hello' hello hello");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("'hello'");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("'he\"l\"lo'");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("test'123\"456\"123'test");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("test\"123'456'123\"test");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup(" test \" 123 ' 456 ' 123 \" test");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup(" test .\". 123 ' 456 ' 123 .\". test");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("cat Makefile|wc -l");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("cat Makefile | wc -l");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("echo $PAGER");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("cat 'Makefile|wc' -l");
-        launch_parser(ms, ms->full_line);
-        ms->full_line = ft_strdup("|wc -l");
-        launch_parser(ms, ms->full_line);
-    }
+		test_quote(ms);
+		test_env(ms);
+		test_pipe(ms);
+	}
 }
