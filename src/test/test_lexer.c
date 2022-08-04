98c602d975ff52c2cb611b585e41f6051bfab92c
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-08-04 13:11:16 +0200

    Free is my life

--- src/test/test_lexer.c
+++ src/test/test_lexer.c
@@ -22,20 +22,28 @@ void test_builtins(t_minishell *ms)
 	printf("===Testing builtins lexer===\n");
     ms->full_line = ft_strdup("echo hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("cd ..");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("pwd hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("export hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("unset hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("env hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("exit hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("history hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
 }
 
 void test_externals(t_minishell *ms)
@@ -43,14 +51,19 @@ void test_externals(t_minishell *ms)
 	printf("===Testing external lexer===\n");
     ms->full_line = ft_strdup("test hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("echo hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("cat cat");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("htop hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("ls -l");
     launch_lexer_command(ms);
+	free(ms->full_line);
 }
 
 void test_pipes(t_minishell *ms)
@@ -58,30 +71,41 @@ void test_pipes(t_minishell *ms)
 	printf("===Testing pipes lexer===\n");
     ms->full_line = ft_strdup("'hello' | hello | hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("cat | | \"cat | cat\"");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("||cd | '$home'");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("'$home'cd | cd | cd");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("|");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("||||");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("echo hi | cat | cat | cat | cat");
     launch_lexer_command(ms);
+	free(ms->full_line);
 }
 
 void test_others(t_minishell *ms)
 {
     ms->full_line = ft_strdup("'hello' hello hello");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("cat \"cat cat\"");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("cd '$home'");
     launch_lexer_command(ms);
+	free(ms->full_line);
     ms->full_line = ft_strdup("'$home'cd cd cd");
     launch_lexer_command(ms);
+	free(ms->full_line);
 }
 
 void test_lexer(t_minishell *ms, int debug)
