f2fba6a17013b0ab7a49bac02222b6626d3a3d35
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-08-10 10:30:34 +0200

    Rework export and unset testing

--- src/test/test_builtin.c
+++ src/test/test_builtin.c
@@ -1,10 +1,49 @@
 #include "../../inc/minishell.h"
 
+static void	display_local_env(t_minishell *ms)
+{
+	t_variable	*env;
+
+	env = ms->first_var;
+	while (env)
+	{
+		ft_printf("%s=%s\n", env->name, env->content);
+		env = env->next;
+	}
+}
+
+static void	launch_unset(t_minishell *ms, char *export, char *unset)
+{
+	ms_export(ms, export);
+	ft_printf("==============================================================\n");
+	ft_printf("Local ENV before UNSET\n");
+	display_local_env(ms);
+	ft_printf("==============================================================\n");
+	ft_printf("Local ENV after UNSET \"%s\"\n", unset);
+	ms_unset(ms, unset);
+	display_local_env(ms);
+	ft_printf("==============================================================\n");
+}
+
 static void	test_unset(t_minishell *ms)
 {
-	ms_export(ms, "export a=1 b=2 c=3 d=4");
-	ms_unset(ms, "unset b c");
-	ms_env(ms);
+	launch_unset(ms, "export a=1 b=2 c=3 d=4", "unset b c");
+	launch_unset(ms, "export best=architect", "unset seeker");
+}
+
+static void	launch_export(t_minishell *ms, char *export)
+{
+	ft_printf("==============================================================\n");
+	ft_printf("Local ENV after EXPORT \"%s\"\n", export);
+	ms_export(ms, export);
+	display_local_env(ms);
+	ft_printf("==============================================================\n");
+}
+
+static void	test_export(t_minishell *ms)
+{
+	launch_export(ms, "export abs=echo ep=$abs");
+	launch_export(ms, "export seeker");
 }
 
 void test_builtin(t_minishell *ms, int debug)
@@ -16,10 +55,7 @@ void test_builtin(t_minishell *ms, int debug)
 	if (debug == TEST_BUILTIN_ENV)
 		ms_env(ms);
 	if (debug == TEST_BUILTIN_EXPORT || debug == TEST_ALL)
-	{
-		ms_export(ms, "export abs=echo e=$abs");
-		ms_env(ms);
-	}
+		test_export(ms);
 	if (debug == TEST_BUILTIN_UNSET)
 		test_unset(ms);
 /*    int test;
