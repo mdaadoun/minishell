74e3b06e0a1579e28c73ab90f2a815da6aa2da06
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-08-30 08:34:51 +0200

    Fix export print when no arg

--- src/core/builtins/ms_export.c
+++ src/core/builtins/ms_export.c
@@ -6,11 +6,27 @@
 /*   By: mdaadoun <mdaadoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 08:46:45 by mdaadoun          #+#    #+#             */
-/*   Updated: 2022/08/22 10:25:06 by dlaidet          ###   ########.fr       */
+/*   Updated: 2022/08/30 08:10:54 by dlaidet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../inc/minishell.h"
+
+static void	print_env_export(t_minishell *ms)
+{
+	t_variable	*env;
+
+	env = ms->first_var;
+	while (env)
+	{
+		ft_putstr_fd("declare -x ", 1);
+		ft_putstr_fd(env->name, 1);
+		ft_putstr_fd("=\"", 1);
+		ft_putstr_fd(env->content, 1);
+		ft_putstr_fd("\"\n", 1);
+		env = env->next;
+	}
+}
 
 static size_t	ft_count(char **str)
 {
@@ -36,7 +52,7 @@ t_uint8	ms_export(t_minishell *ms, char **arg)
 
 	ind = ft_count(arg);
 	if (ind == 1)
-		ms_env(ms);
+		print_env_export(ms);
 	else
 	{
 		ind = 1;
