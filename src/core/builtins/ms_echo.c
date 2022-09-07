46df124dc5c9bdb26bfdcf7db91f8ae6cf667084
Dylan Laidet <dlaidet@1A7.42angouleme.fr>
2022-09-07 07:49:08 +0200

    ms_echo patched and pass the NORM

--- src/core/builtins/ms_echo.c
+++ src/core/builtins/ms_echo.c
@@ -6,7 +6,7 @@
 /*   By: mdaadoun <mdaadoun@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 08:46:45 by mdaadoun          #+#    #+#             */
-/*   Updated: 2022/09/06 17:51:10 by mdaadoun         ###   ########.fr       */
+/*   Updated: 2022/09/07 07:47:11 by dlaidet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,46 +22,49 @@ static int	nb_arg(char **arg)
 	return (ind);
 }
 
+static void	print_echo(char **arg, int ind)
+{
+	while (arg[ind])
+	{
+		ft_printf("%s", arg[ind]);
+		if (arg[ind + 1] && arg[ind][0] != '\0')
+			ft_putchar_fd(' ', 1);
+		ind++;
+	}
+}
+
+static bool	check_arg(char **arg, int ind)
+{
+	int	i;
+
+	i = 1;
+	while (arg[ind][i] && arg[ind][i] == 'n')
+		i++;
+	if (arg[ind][i] != 'n')
+		return (true);
+	return (false);
+}
+
 // display a line of text
 void	ms_echo(char **arg)
 {
-	int	ind;
-	int	n;
-	int	i;
+	int		ind;
+	bool	n;
 
 	ind = 1;
-	n = 0;
+	n = true;
 	if (nb_arg(arg) > 1)
 	{
-
-		// while (arg[ind] && ft_strncmp(arg[ind], "-n", 2) == 0)
-		// {
-		// 	n = 1;
-		// 	ind++;
-		// }
-		if (arg[ind][0] == '-')
+		while (arg[ind][0] == '-')
 		{
-			i = 1;
-			while (arg[ind][i])
-			{
-				n = 1;
-				if (arg[ind][i] != 'n')
-				{
-					n = 0;
-					break;
-				}
-				i++;
-			}
-		}
-		while (arg[ind])
-		{
-			ft_printf("%s", arg[ind]);
-			if (arg[ind + 1] && arg[ind][0] != '\0')
-				ft_putchar_fd(' ', 1);
+			n = check_arg(arg, ind);
+			if (n == true)
+				break ;
 			ind++;
 		}
+		print_echo(arg, ind);
 	}
-	if (n == 0)
+	if (n == true)
 		ft_putchar_fd('\n', 1);
 	g_sig.exit_status = 0;
 }
